fe1fb67e0dc6a0ca1622feb2e1852483b9c0a6fd
Kieferer <balazsborsi0@gmail.com>
2023-08-24 01:30:35 +0200

    feat: Create PlatformDataBuilder class to be able to instantiate objects as Builder design pattern suggests

---
+++ Builders/PlatformDataBuilder.cs
@@ -0,0 +1,27 @@
+﻿using UAIAPI.Models;
+
+namespace UAIAPI.Builders
+{
+    public class PlatformDataBuilder
+    {
+        private string? Signature;
+        private string? Url;
+
+        public PlatformDataBuilder SetSignature(string signature)
+        {
+            Signature = signature;
+            return this;
+        }
+
+        public PlatformDataBuilder SetUrl(string url)
+        {
+            Url = url;
+            return this;
+        }
+
+        public PlatformData Build()
+        {
+            return new PlatformData(Signature, Url);
+        }
+    }
+}

710d21afec7d0db649b0acc3c6c619e1df9de417
Kieferer <balazsborsi0@gmail.com>
2023-08-24 01:29:16 +0200

    feat: Create ReleaseData class to store every release information of a release platfrom included

---
+++ Models/ReleaseData.cs
@@ -0,0 +1,18 @@
+﻿namespace UAIAPI.Models
+{
+    public class ReleaseData
+    {
+        public string Version { get; }
+        public string Notes { get; }
+        public DateTime PubDate { get; }
+        public Dictionary<string, PlatformData> Platforms { get; }
+
+        public ReleaseData(string version, string notes, Dictionary<string, PlatformData> platforms)
+        {
+            Version = version;
+            Notes = notes;
+            PubDate = DateTime.Now;
+            Platforms = platforms;
+        }
+    }
+}

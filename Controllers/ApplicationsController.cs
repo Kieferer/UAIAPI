38a8d84dd70f18b2802322755ea24d7ebae4d5c6
Kieferer <balazsborsi0@gmail.com>
2023-08-23 18:20:46 +0200

    feat: Create ApplicationsController

---
+++ Controllers/ApplicationsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace UAIAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApplicationsController : Controller
+    {
+        public HttpStatusCode Get()
+        {
+            return HttpStatusCode.OK;
+        }
+    }
+}

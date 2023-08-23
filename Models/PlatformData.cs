adbd642182a7525aa288c129b454b2e247207a88
Kieferer <balazsborsi0@gmail.com>
2023-08-24 01:28:41 +0200

    feat: Create PlatformData class to store tauri-app platform details

---
+++ Models/PlatformData.cs
@@ -0,0 +1,14 @@
+﻿namespace UAIAPI.Models
+{
+    public class PlatformData
+    {
+        public string Signature { get; }
+        public string Url { get; }
+
+        public PlatformData(string signature, string url)
+        {
+            Signature = signature;
+            Url = url;
+        }
+    }
+}

90ce9da51a865afd9570522314d6fd98b4a38257
Kieferer <balazsborsi0@gmail.com>
2023-08-24 01:30:46 +0200

    feat: Create ReleaseDataBuilder class to be able to instantiate objects as Builder design pattern suggests

---
+++ Builders/ReleaseDataBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UAIAPI.Models;
+
+namespace UAIAPI.Builders
+{
+    public class ReleaseDataBuilder
+    {
+        private string? Version;
+        private string? Notes;
+        private Dictionary<string, PlatformData> Platforms = new Dictionary<string, PlatformData>();
+
+        public ReleaseDataBuilder SetVersion(string version)
+        {
+            Version = version;
+            return this;
+        }
+
+        public ReleaseDataBuilder SetNotes(string notes)
+        {
+            Notes = notes;
+            return this;
+        }
+
+        public ReleaseDataBuilder SetPlatform(Dictionary<string, PlatformData> platforms)
+        {
+            Platforms = platforms;
+            return this;
+        }
+
+        public ReleaseDataBuilder AddPlatform(string platform, PlatformData platformInfo)
+        {
+            Platforms.Add(platform, platformInfo);
+            return this;
+        }
+
+        public ReleaseData Build()
+        {
+            return new ReleaseData(Version, Notes, Platforms);
+        }
+    }
+}

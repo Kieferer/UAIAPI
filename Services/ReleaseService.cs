9c0223890f2b185c4c708f30c357d3a8dd19bca3
Kieferer <balazsborsi0@gmail.com>
2023-08-24 01:49:15 +0200

    feat: Implement SetOrUpdateRelease and GetReleaseData method in ReleaseService

--- Services/ReleaseService.cs
+++ Services/ReleaseService.cs
@@ -9,6 +9,17 @@ namespace UAIAPI.Services
         {
             projectReleases = new Dictionary<string, ReleaseData>();
         }
-
+        public void SetOrUpdateRelease(string name,  ReleaseData release)
+        {
+            projectReleases[name] = release;
+        }
+        public ReleaseData? GetReleaseData(string projectName)
+        {
+            if (projectReleases.TryGetValue(projectName, out ReleaseData? releaseData))
+            {
+                return releaseData;
+            }
+            return null;
+        }
     }
 }

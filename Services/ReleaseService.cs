2d1fe7dbd6f1840027de88dd8869143c798135ef
Kieferer <balazsborsi0@gmail.com>
2023-08-24 01:39:12 +0200

    feat: Create projectReleases Dictionary in ReleaseService to store ReleaseDatas of tauri-apps assigned to them by their name

--- Services/ReleaseService.cs
+++ Services/ReleaseService.cs
@@ -1,6 +1,10 @@
-﻿namespace UAIAPI.Services
+﻿using UAIAPI.Models;
+
+namespace UAIAPI.Services
 {
     public class ReleaseService
     {
+        private Dictionary<string, ReleaseData> projectData = new Dictionary<string, ReleaseData>();
+
     }
 }

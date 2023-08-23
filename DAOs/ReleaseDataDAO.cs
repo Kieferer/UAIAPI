067a7cab9a3145a3d2419f43d02c3ace6148477b
Kieferer <balazsborsi0@gmail.com>
2023-08-24 01:32:14 +0200

    feat: Create ReleaseDataDAO class, because incoming JSON should contain the project name, but tauri-app does not need it for tauri-app just the ReleaseService

---
+++ DAOs/ReleaseDataDAO.cs
@@ -0,0 +1,22 @@
+﻿using UAIAPI.Models;
+
+namespace UAIAPI.DAOs
+{
+    public class ReleaseDataDAO
+    {
+        public string Name { get; }
+        public string Version { get; }
+        public string Notes { get; }
+        public DateTime PubDate { get; }
+        public Dictionary<string, PlatformData> Platforms { get; }
+
+        public ReleaseDataDAO(string name, string version, string notes, Dictionary<string, PlatformData> platforms)
+        {
+            Name = name;
+            Version = version;
+            Notes = notes;
+            PubDate = DateTime.Now;
+            Platforms = platforms;
+        }
+    }
+}

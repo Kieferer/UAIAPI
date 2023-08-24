8c85d4a63a135d2dd6c3ab5b0156f7396cd8c224
Kieferer <balazsborsi0@gmail.com>
2023-08-24 02:11:39 +0200

    feat: Add ReleaseService as singleton

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using UAIAPI.Services;
+
 namespace UAIAPI
 {
     public class Program
@@ -6,18 +8,13 @@ namespace UAIAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            builder.Services.AddSingleton<ReleaseService>();
             builder.Services.AddControllers();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
 
             app.MapControllers();
 

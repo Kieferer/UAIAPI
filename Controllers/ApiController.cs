8b03794b3ccfc090cb4ee4be2f2e2886ad8b9fa0
Kieferer <balazsborsi0@gmail.com>
2023-08-24 02:11:11 +0200

    feat: Implement GetUpdateData method

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Net;
 using UAIAPI.Builders;
 using UAIAPI.DAOs;
 using UAIAPI.Models;
+using UAIAPI.Services;
 
 namespace UAIAPI.Controllers
 {
@@ -13,7 +11,12 @@ namespace UAIAPI.Controllers
     [Route("[controller]")]
     public class ApiController : Controller
     {
-        Dictionary<string, ReleaseData> projectData = new Dictionary<string, ReleaseData>();
+        private readonly ReleaseService releaseService;
+        public ApiController(ReleaseService releaseService)
+        {
+            this.releaseService = releaseService;
+        }
+     
         [HttpPost("update")]
         public IActionResult SetUpdateData([FromBody] ReleaseDataDAO releaseDataDAO)
         {
@@ -21,29 +24,28 @@ namespace UAIAPI.Controllers
                 .SetVersion(releaseDataDAO.Version)
                 .SetNotes(releaseDataDAO.Notes)
                 .SetPlatform(releaseDataDAO.Platforms)
-                .Build();
+            .Build();
 
-            string output;
-            string NameOfProject = releaseDataDAO.Name;
-            if (projectData.ContainsKey(NameOfProject))
-            {
-                projectData[NameOfProject] = releaseData;
-                output = $"${NameOfProject} is updated in the server with its latest (${releaseData.Version}) version. Stored projects: ${projectData.Keys.Count}";
-            }
-            else
-            {
-                projectData.Add(releaseDataDAO.Name, releaseData);
-                output = $"${NameOfProject} is registered to the server with its latest (${releaseData.Version}) version. Stored projects: ${projectData.Keys.Count}";
-            }
+            releaseService.SetOrUpdateRelease(releaseDataDAO.Name, releaseData);
 
-            //string BackToJSONFormatForTest = JsonConvert.SerializeObject(releaseData, Formatting.Indented);
-            return Content(output, "application/text");
+            string output = $"${releaseDataDAO.Name} is updated in the server with its latest (${releaseData.Version}) version." +
+                $"Stored projects: ${releaseService.GetProjectCount()}";
+
+            return Content(output, "text/plain");
         }
 
-        [HttpGet("update")]
-        public IActionResult GetUpdateData()
+        [HttpGet("update/{projectName}")]
+        public IActionResult GetUpdateData(string projectName)
         {
-            throw new NotImplementedException();
+            var releaseData = releaseService.GetReleaseData(projectName);
+
+            if (releaseData == null)
+            {
+                return NotFound();
+            }
+
+            string serializedReleaseData = JsonConvert.SerializeObject(releaseData, Formatting.Indented);
+            return Content(serializedReleaseData, "application/json");
         }
     }
 }

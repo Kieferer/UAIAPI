41d0a0629f88d72ce1622e790951e762844faed5
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-08-24 13:05:03 +0200

    refactor: Rename variables to match with the JSON tags of tauri-updater

--- Models/ReleaseData.cs
+++ Models/ReleaseData.cs
@@ -2,17 +2,17 @@
 {
     public class ReleaseData
     {
-        public string Version { get; }
-        public string Notes { get; }
-        public DateTime PubDate { get; }
-        public Dictionary<string, PlatformData> Platforms { get; }
+        public string version { get; }
+        public string notes { get; }
+        public DateTime pub_date { get; }
+        public Dictionary<string, PlatformData> platforms { get; }
 
         public ReleaseData(string version, string notes, Dictionary<string, PlatformData> platforms)
         {
-            Version = version;
-            Notes = notes;
-            PubDate = DateTime.Now;
-            Platforms = platforms;
+            this.version = version;
+            this.notes = notes;
+            this.pub_date = DateTime.Now;
+            this.platforms = platforms;
         }
     }
 }

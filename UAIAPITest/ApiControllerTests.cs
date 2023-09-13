a99dcc2369f4ae3effa60d5801414b487b1be6f7
Kieferer <balazsborsi0@gmail.com>
2023-09-13 16:28:37 +0200

    feat: Create 'mockReleaseService' and 'apiController' variables for tests on the controller

--- UAIAPITest/ApiControllerTests.cs
+++ UAIAPITest/ApiControllerTests.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UAIAPI.Controllers;
+using UAIAPI.Models;
+using UAIAPI.Services;
 
 namespace UAIAPITest
 {
     internal class ApiControllerTests
     {
+        private Mock<ReleaseService> mockReleaseService;
+        private ApiController apiController;
 
+        [SetUp]
+        public void Setup()
+        {
+            mockReleaseService = new Mock<ReleaseService>();
+            apiController = new ApiController(mockReleaseService.Object);
+        }
     }
 }

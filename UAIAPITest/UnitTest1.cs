3059294a0da3d33fb2171df853eb3610727fefbd
Kieferer <balazsborsi0@gmail.com>
2023-09-13 15:17:25 +0200

    feat: Create readonly string variables for test contuctors of model objects and builders

--- UAIAPITest/UnitTest1.cs
+++ UAIAPITest/UnitTest1.cs
@@ -2,6 +2,10 @@ namespace UAIAPITest
 {
     public class Tests
     {
-        
+        readonly string version = "testVersion";
+        readonly string notes = "testNotes";
+        readonly string signature = "testSig";
+        readonly string url = "testURL";
+        readonly string platform = "testPlatform";
     }
 }

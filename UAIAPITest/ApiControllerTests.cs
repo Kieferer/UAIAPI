aeabe960c8f906bf3d65ed0453c746949ebe296d
Kieferer <balazsborsi0@gmail.com>
2023-09-13 16:29:26 +0200

    feat: Create variables for tests and initialize dictionary in 'Setup' method

--- UAIAPITest/ApiControllerTests.cs
+++ UAIAPITest/ApiControllerTests.cs
@@ -15,11 +15,18 @@ namespace UAIAPITest
         private Mock<ReleaseService> mockReleaseService;
         private ApiController apiController;
 
+        readonly string appName = "testAppName";
+        readonly string version = "testVersion";
+        readonly string notes = "testNotes";
+
+        Dictionary<string, PlatformData> appReleaseData;
+
         [SetUp]
         public void Setup()
         {
             mockReleaseService = new Mock<ReleaseService>();
             apiController = new ApiController(mockReleaseService.Object);
+            appReleaseData = new Dictionary<string, PlatformData>();
         }
     }
 }

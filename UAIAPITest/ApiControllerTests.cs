38536b88daaa9c840854a7e420b2e923a7e8f824
Kieferer <balazsborsi0@gmail.com>
2023-09-13 16:30:53 +0200

    feat: Create 'TestSetUpdateData' to test adding meta data of projects to the controller by its 'SetUpdateData' method

--- UAIAPITest/ApiControllerTests.cs
+++ UAIAPITest/ApiControllerTests.cs
@@ -1,10 +1,12 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UAIAPI.Controllers;
+using UAIAPI.DAOs;
 using UAIAPI.Models;
 using UAIAPI.Services;
 
@@ -27,6 +29,17 @@ namespace UAIAPITest
             mockReleaseService = new Mock<ReleaseService>();
             apiController = new ApiController(mockReleaseService.Object);
             appReleaseData = new Dictionary<string, PlatformData>();
+        }
+
+        [Test]
+        public void TestSetUpdateData()
+        {
+            var releaseDataDAO = new ReleaseDataDAO(appName, version, notes, appReleaseData);
+
+            var result = apiController.SetUpdateData(releaseDataDAO);
+
+            Assert.IsInstanceOf<ContentResult>(result);
+            mockReleaseService.Verify(service => service.SetOrUpdateRelease(It.IsAny<string>(), It.IsAny<ReleaseData>()), Times.Once);
         }
     }
 }

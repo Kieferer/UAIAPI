187ec19c21aa1698b6f800bc8feb7c640e6e2971
Kieferer <balazsborsi0@gmail.com>
2023-09-13 16:35:18 +0200

    feat: Create 'releaseService' variable for tests and initialize it in 'Setup' method

--- UAIAPITest/ReleaseServiceTests.cs
+++ UAIAPITest/ReleaseServiceTests.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UAIAPI.Services;
 
 namespace UAIAPITest
 {
     internal class ReleaseServiceTests
     {
+        private ReleaseService releaseService;
 
+        [SetUp]
+        public void Setup()
+        {
+            releaseService = new ReleaseService();
+        }
     }
 }
